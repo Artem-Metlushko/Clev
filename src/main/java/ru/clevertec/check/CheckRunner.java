b2faf047f5347a09a17acb28d4b21751a7b584c7
“Artem <3947710@gmail.com>
2024-07-04 09:27:11 +0300

    first commit

---
+++ src/main/java/ru/clevertec/check/CheckRunner.java
@@ -0,0 +1,9 @@
+package ru.clevertec.check;
+
+public class CheckRunner {
+    private Long id;
+    private Integer quantity;
+
+    private Integer discountCard;
+    private Integer balanceDebitCard;
+}
